c2a94a9cc004210507c32a7cf034737de0f51532
Meheraan Khan <mhk7451@rit.edu>
2024-07-29 08:33:40 -0400

    chatbox

--- ai_model_ven/src/index.py
+++ ai_model_ven/src/index.py
@@ -1,58 +1,36 @@
 import os
 import sys
-import logging.config
-import yaml
-# import plugnplai as pl
+import logging
+import json
 from llama.core.llama import Llama
 
 # Basic configuration for logger
-logging.config.dictConfig(yaml.load(open('config/logger_config.yaml', 'r'), Loader=yaml.FullLoader))
-
-# Now you can use logger in your script
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-logger.info('logger configured using YAML file.')
-logger.info("logger initialized.")  # Example log message
+logger.info("Logger initialized.")  # Example log message
 
-
-def main()-> None:
-
-    model = input("What AI model would you like it: \n << LLAMA (1) >>\n << ChatGPT (1) >>? ")
-
-    match model:
-        case 1:
-            print("You can become a web developer.")
-
-        case 2:
-            print("You can become a Data Scientist")
-
-        case 3:
-            print("You can become a backend developer")
-        
-        case 4:
-            print("You can become a Blockchain developer")
-
-        case 5:
-            print("You can become a mobile app developer")
-        case _:
-            print("The language doesn't matter, what matters is solving problems.")
-
-# Example usage:
-if __name__ == "__main__":
-    # Accessing the API key
-    api_key = os.getenv('LLAMA_API_KEY')
-
+def main():
     if api_key is None:
         logger.error("API_KEY environment variable not set")
         sys.exit(1)  # Exit the program if API_KEY is not set
 
+    client = Llama(api_key=api_key, user_message="")
 
-    # user input testing porpuse
-    # user_input = input("Enter your question: ")
+    while True:
+        user_input = input("Type your message or enter Q to quit: ")
+        if user_input.upper() == "Q":
+            break
 
-    # Create an instance of LlamaAPI
-    api_client = Llama(" 1 + 1 ?")
+        client.update_user_message(user_input)
+        response = client.execute_request()
 
-    # Execute the API request
-    # response = api_client.execute_request()
-    # print(f"Response: {response}")
+        if response:
+            response_json = json.dumps(response, indent=2)
+            print(response_json)
+        else:
+            logger.error("No response from Llama API")
 
+if __name__ == "__main__":
+    # Accessing the API key
+    api_key = "LL-yAOWPfOpKp5DaXnHpJD8L46AXw4uKRC0kcnHQH9toSzf4tuDzM9MJvxaYz2MOjJZ"
+    main()
--- ai_model_ven/src/llama/core/llama.py
+++ ai_model_ven/src/llama/core/llama.py
@@ -1,57 +1,29 @@
 import json
-# import logger
+import logging
 from llamaapi import LlamaAPI  
 import os
-import sys
 import attr
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-
-
-logger.info("logger initialized.")  # Example log message
+#logger.info("Logger initialized.")  # Example log message
 
 @attr.s
 class Llama:
-    """
-    Represents a Llama object that interacts with the LlamaAPI.
-
-    Args:
-        user_message (str): The user message to be sent to the LlamaAPI.
-        api_key (str, optional): The API key for authentication. Defaults to the value of the 'LLAMA_API_KEY' environment variable.
-        role (str, optional): The role of the user. Defaults to "user".
-        config_file_path (str, optional): The path to the configuration file. Defaults to 'config.json'.
-
-    Attributes:
-        user_message (str): The user message to be sent to the LlamaAPI.
-        api_key (str): The API key for authentication.
-        llama (LlamaAPI): The LlamaAPI instance.
-        role (str): The role of the user.
-        config_file_path (str): The path to the configuration file.
-        api_request_json (dict): The API request JSON structure with the configured parameters.
-    """
-
     user_message = attr.ib()
-    api_key = attr.ib(default=os.getenv('LLAMA_API_KEY'))  # Encapsulated variable for API key
+    api_key = attr.ib(default=os.getenv('LLAMA_API_KEY'))
     llama = attr.ib(init=False)
     role = attr.ib(default="user")
     config_file_path = attr.ib(default='config.json')
     api_request_json = attr.ib(init=False)
 
     def __attrs_post_init__(self):
-        """
-        Initializes the Llama object by creating a LlamaAPI instance and setting the API request JSON structure.
-        """
         self.llama = LlamaAPI(self.api_key) if self.api_key else None
+        logger.debug(f"LlamaAPI instance: {self.llama}")
         self.api_request_json = self._initialize_api_config()
 
     def _initialize_api_config(self):
-        """
-        Initializes the API configuration by loading the configuration file and setting the necessary parameters.
-
-        Returns:
-            dict: The API request JSON structure with the configured parameters.
-        """
-        # Define default API request JSON structure
         api_request_json = {
             "messages": [
                 {
@@ -62,94 +34,79 @@ class Llama:
             "functions": [], 
             "model": "",  
             "stream": "", 
-            "max_token": "", 
-            "temperature": "",
-            "top_p": "",
-            "frequency_penalty": ""
+            "max_token": 500, 
+            "temperature": .2,
+            "top_p": 1.0,
+            "frequency_penalty": 1.0
         }
 
-        self.config_file_path = os.getcwd() + f"/llama/{self.config_file_path}"
-        print(f"absolute_path:  {self.config_file_path}")
+        config_file_path = os.path.join(os.getcwd(), "llama", "config", self.config_file_path)
+        #logger.info(f"Constructed config file path: {self.config_file_path}")
 
-        sys.exit()
-        # Load the configuration file
-        if os.path.exists(self.config_file_path):
-            # File exists, so open it and load the configuration
-            with open(self.config_file_path, 'r') as config_file:
+        if os.path.exists(config_file_path):
+            with open(config_file_path, 'r') as config_file:
                 config = json.load(config_file)
-                logger.info(f"Config File Contains: {config}")
+                #logger.info(f"Config File Contains: {config}")
+                
+                api_request_json.update({
+                    "model": config.get("model", ""),
+                    "stream": config.get("stream", ""),
+                    "max_token": config.get("max_token", 500),
+                    "temperature": config.get("temperature", 0.2),
+                    "top_p": config.get("top_p", 1.0),
+                    "frequency_penalty": config.get("frequency_penalty", 1.0),
+                })
+                
+                functions = config.get('functions', [])
+                for function in functions:
+                    function_call = {
+                        "name": function.get('name', ''),
+                        "parameters": {
+                            "type": "object",
+                            "properties": function.get('parameters', {})
+                        },
+                        "required": function.get('required', [])
+                    }
+                    api_request_json['functions'].append(function_call)
         else:
-            logger.error(f"The configuration file '{self.config_file_path}' does not exist.")
-            # Handle this error scenario as needed
+            logger.error(f"The configuration file '{config_file_path}' does not exist.")
             return None
-
-        # Set functions by extracting from config
-        functions = config.get('functions', [])
-        for function in functions:
-            function_call = {
-                "name": function.get('name', ''),
-                "parameters": {
-                    "type": "object",
-                    "properties": function.get('parameters', {})
-                },
-                "required": function.get('required', [])
-            }
-            
-            api_request_json['functions'].append(function_call)
-
-        # Set the model from config
-        api_request_json["model"] = config.get("model", "")
-        api_request_json["stream"] = config.get("stream", False)
-        api_request_json["max_token"] = config.get("max_token", 500)
-        api_request_json["temperature"] = config.get("temperature", 0.2)
-        api_request_json["top_p"] = config.get("top_p", 1.0)
-        api_request_json["frequency_penalty"] = config.get("frequency_penalty", 1.0)
 
         return api_request_json
 
     def execute_request(self):
-        """
-        Executes the API request using the LlamaAPI instance and returns the response.
-
-        Returns:
-            dict: The response received from the API.
-        """
-        try:
-            logger.info("Sending API request:")
-            # logger.info(json.dumps(self.api_request_json, indent=2))
-
-            if self.llama:
-                # Execute the request using your SDK or library (assuming llama.run() exists)
-                response = self.llama.run(self.api_request_json)
-                print(f"Response: {response}")
-
-                # Log the response received
-                logger.info("API request successful")
-                logger.info(f"Response received: {response}")
-                # logger.info(json.dumps(response, indent=2))
-
-                return response
-            else:
-                logger.error("LlamaAPI instance not initialized.")
-                return None
-
-        except Exception as e:
-            logger.error(f"Error executing API request: {str(e)}")
-            # Handle the error appropriately, e.g., retry, notify user, etc.
+        if not self.llama:
+            logger.error("LlamaAPI instance not initialized.")
             return None
 
-# Example usage:
+        try:
+            logger.info("Sending API request:")
+            message = self.api_request_json.get('messages', [])
+            for m in message:
+                print(f"Role: {m.get('role')}")
+                print(f"Content: {m.get('content')}")
+                print()
+            response = self.llama.run(self.api_request_json).json()
+            # Check if the response is a stream or needs different handling
+            if isinstance(response, (str, dict)):  # Handle if response is JSON or string
+                #logger.info(f"Response received: {response}")
+                logger.info("Response received:\n%s", json.dumps(response, indent=4))
+                #logger.info("response received")
+                return response
+            else:
+                logger.warning("Received unexpected response type.")
+                return None
+        except Exception as e:
+            logger.error(f"Error executing API request: {str(e)}")
+            return None
+        
+    def update_user_message(self, msg):
+        self.user_message = msg
+        self.api_request_json = self._initialize_api_config()
+
 if __name__ == "__main__":
-    # Accessing the API key
-    api_key = os.getenv('LLAMA_API_KEY')
-
-    if api_key is None:
-        logger.error("API_KEY environment variable not set")
-        sys.exit(1)  # Exit the program if API_KEY is not set
-
-    # Create an instance of LlamaAPI
-    api_client = Llama("What is the weather today?")
-
-    # Execute the API request
+    api_key = "LL-yAOWPfOpKp5DaXnHpJD8L46AXw4uKRC0kcnHQH9toSzf4tuDzM9MJvxaYz2MOjJZ"
+    api_client = Llama("What is the weather today?", api_key=api_key)
     response = api_client.execute_request()
-    print(f"Response: {response}")
+    # for choice in response.get('choices'):
+    #     print(choice['message']['content'])
